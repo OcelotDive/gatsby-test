dcf7f4f7ab54711d18f2d5b14f461d9b50374aa2
ocelotdive <oscar1dlh@hotmail.com>
2020-08-25 14:06:19 +0100

    add: graph component

--- src/components/companyDetails/CompanyDataBrief.js
+++ src/components/companyDetails/CompanyDataBrief.js
@@ -2,7 +2,8 @@ import React from "react";
 
 import companyStyles from "./company.module.css";
 const CompanyDataBrief = ({company, companyKeyMetrics}) => {
-    
+    const keyMetrics = companyKeyMetrics.metrics[0];
+    console.log(keyMetrics)
     return (
         <>
         <section className={companyStyles.companyDataBrief}>
@@ -34,13 +35,49 @@ const CompanyDataBrief = ({company, companyKeyMetrics}) => {
             </div>
             <div className={companyStyles.infoBar}>
                 <div className={companyStyles.dataName}>Div Yield</div>
-                <div className={companyStyles.dataInfo}>{companyKeyMetrics.metrics[0]['Dividend Yield']}</div>
+                <div className={companyStyles.dataInfo}>{keyMetrics['Dividend Yield']}</div>
             </div>
             <div className={companyStyles.infoBar}>
                 <div className={companyStyles.dataName}>PE Ratio</div>
-                <div className={companyStyles.dataInfo}>{companyKeyMetrics.metrics[0]['PE ratio'].substring(0, 5)}</div>
+                <div className={companyStyles.dataInfo}>{keyMetrics['PE ratio'].substring(0, 5)}</div>
             </div>
             </div>
+            <div className={companyStyles.companyDataSection}>
+            <div className={companyStyles.dataBriefHeaderSection}></div>
+            <div className={companyStyles.infoBar}>
+                <div className={companyStyles.dataName}>PB Ratio</div>
+                <div className={companyStyles.dataInfo}>{keyMetrics['PB ratio'].substring(0, 5)}</div>
+            </div>
+            <div className={companyStyles.infoBar}>
+                <div className={companyStyles.dataName}>E/Yield</div>
+                <div className={companyStyles.dataInfo}>{keyMetrics['Earnings Yield'].substring(0, 5)}</div>
+            </div>
+            <div className={companyStyles.infoBar}>
+                <div className={companyStyles.dataName}>Debt/Equity</div>
+                <div className={companyStyles.dataInfo}>{keyMetrics['Debt to Equity'].substring(0, 5)}</div>
+            </div>
+            <div className={companyStyles.infoBar}>
+                <div className={companyStyles.dataName}>ROE</div>
+                <div className={companyStyles.dataInfo}>{(keyMetrics['ROE'] * 100).toFixed(2)}%</div>
+            </div>
+            <div className={companyStyles.infoBar}>
+                <div className={companyStyles.dataName}>W/Cap</div>
+                <div className={companyStyles.dataInfo}>{(keyMetrics['Working Capital'] / 1000000000).toFixed(2)}B</div>
+            </div>
+            <div className={companyStyles.infoBar}>
+                <div className={companyStyles.dataName}>TA Value</div>
+                <div className={companyStyles.dataInfo}>{(keyMetrics['Tangible Asset Value'] / 1000000000).toFixed(2)}B</div>
+            </div>
+            <div className={companyStyles.infoBar}>
+                <div className={companyStyles.dataName}>N/C AV</div>
+                <div className={companyStyles.dataInfo}>{(keyMetrics['Net Current Asset Value'] / 1000000000).toFixed(2)}B</div>
+            </div>
+            <div className={companyStyles.infoBar}>
+                <div className={companyStyles.dataName}>Inv Cap</div>
+                <div className={companyStyles.dataInfo}>{(keyMetrics['Invested Capital'] / 1000000000).toFixed(2)}B</div>
+            </div>
+            </div>
+            
 
         </section>
         </>
--- src/pages/company-page.js
+++ src/pages/company-page.js
@@ -10,10 +10,286 @@ import { useLocation } from '@reach/router';
 import queryString from 'query-string'
 
 import companyStyles from "../components/companyDetails/company.module.css";
+import CompanyLineChart from "../components/companyDetails/CompanyLineChart";
 //return this.http.get(`${this.companyProfile}${symbol}${this.fmpk}`)
 //this.keyMetrics = 'https://financialmodelingprep.com/api/v3/company-key-metrics/'
 
 const CompanyPage = () => {
+
+
+
+  const data = [
+    {
+      "id": "japan",
+      "color": "hsl(223, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 293
+        },
+        {
+          "x": "helicopter",
+          "y": 143
+        },
+        {
+          "x": "boat",
+          "y": 161
+        },
+        {
+          "x": "train",
+          "y": 261
+        },
+        {
+          "x": "subway",
+          "y": 206
+        },
+        {
+          "x": "bus",
+          "y": 20
+        },
+        {
+          "x": "car",
+          "y": 5
+        },
+        {
+          "x": "moto",
+          "y": 82
+        },
+        {
+          "x": "bicycle",
+          "y": 149
+        },
+        {
+          "x": "horse",
+          "y": 90
+        },
+        {
+          "x": "skateboard",
+          "y": 208
+        },
+        {
+          "x": "others",
+          "y": 121
+        }
+      ]
+    },
+    {
+      "id": "france",
+      "color": "hsl(92, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 255
+        },
+        {
+          "x": "helicopter",
+          "y": 3
+        },
+        {
+          "x": "boat",
+          "y": 25
+        },
+        {
+          "x": "train",
+          "y": 130
+        },
+        {
+          "x": "subway",
+          "y": 289
+        },
+        {
+          "x": "bus",
+          "y": 220
+        },
+        {
+          "x": "car",
+          "y": 137
+        },
+        {
+          "x": "moto",
+          "y": 51
+        },
+        {
+          "x": "bicycle",
+          "y": 276
+        },
+        {
+          "x": "horse",
+          "y": 2
+        },
+        {
+          "x": "skateboard",
+          "y": 79
+        },
+        {
+          "x": "others",
+          "y": 136
+        }
+      ]
+    },
+    {
+      "id": "us",
+      "color": "hsl(193, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 6
+        },
+        {
+          "x": "helicopter",
+          "y": 241
+        },
+        {
+          "x": "boat",
+          "y": 210
+        },
+        {
+          "x": "train",
+          "y": 21
+        },
+        {
+          "x": "subway",
+          "y": 17
+        },
+        {
+          "x": "bus",
+          "y": 10
+        },
+        {
+          "x": "car",
+          "y": 291
+        },
+        {
+          "x": "moto",
+          "y": 75
+        },
+        {
+          "x": "bicycle",
+          "y": 56
+        },
+        {
+          "x": "horse",
+          "y": 240
+        },
+        {
+          "x": "skateboard",
+          "y": 232
+        },
+        {
+          "x": "others",
+          "y": 126
+        }
+      ]
+    },
+    {
+      "id": "germany",
+      "color": "hsl(232, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 112
+        },
+        {
+          "x": "helicopter",
+          "y": 35
+        },
+        {
+          "x": "boat",
+          "y": 258
+        },
+        {
+          "x": "train",
+          "y": 89
+        },
+        {
+          "x": "subway",
+          "y": 154
+        },
+        {
+          "x": "bus",
+          "y": 179
+        },
+        {
+          "x": "car",
+          "y": 237
+        },
+        {
+          "x": "moto",
+          "y": 175
+        },
+        {
+          "x": "bicycle",
+          "y": 209
+        },
+        {
+          "x": "horse",
+          "y": 273
+        },
+        {
+          "x": "skateboard",
+          "y": 30
+        },
+        {
+          "x": "others",
+          "y": 179
+        }
+      ]
+    },
+    {
+      "id": "norway",
+      "color": "hsl(9, 70%, 50%)",
+      "data": [
+        {
+          "x": "plane",
+          "y": 188
+        },
+        {
+          "x": "helicopter",
+          "y": 285
+        },
+        {
+          "x": "boat",
+          "y": 151
+        },
+        {
+          "x": "train",
+          "y": 9
+        },
+        {
+          "x": "subway",
+          "y": 4
+        },
+        {
+          "x": "bus",
+          "y": 68
+        },
+        {
+          "x": "car",
+          "y": 295
+        },
+        {
+          "x": "moto",
+          "y": 246
+        },
+        {
+          "x": "bicycle",
+          "y": 241
+        },
+        {
+          "x": "horse",
+          "y": 206
+        },
+        {
+          "x": "skateboard",
+          "y": 188
+        },
+        {
+          "x": "others",
+          "y": 142
+        }
+      ]
+    }
+  ];
 
   const location = useLocation();
   console.log("myNewLocation", location)
@@ -25,9 +301,11 @@ const CompanyPage = () => {
      let [companyObject, setCompanyObject] = useState({});
      let [companyRating, setCompanyRating] = useState({});
      let [companyKeyMetrics, setCompanyKeyMetrics] = useState({});
+     let [companyHistorical, setCompanyHistorical] = useState([]);
      const companyUrl = 'https://financialmodelingprep.com/api/v3/company/profile/';
      const companyRatingsUrl = 'https://financialmodelingprep.com/api/v3/company/rating/';
      const keyMetricsUrl = 'https://financialmodelingprep.com/api/v3/company-key-metrics/';
+     const companyHistoricalUrl = 'https://financialmodelingprep.com/api/v3/historical-price-full/';
 
      useEffect(() => {
       const fetchData = async () => {
@@ -50,23 +328,74 @@ const CompanyPage = () => {
     useEffect(() => {
       const fetchData = async () => {
         const result = await axios(`${keyMetricsUrl}${symbol}${Key.fmpk}`,);
-        console.log("key-metrics", result.data)
+     //   console.log("key-metrics", result.data)
         setCompanyKeyMetrics(result.data);
         }
         fetchData();
           }, [symbol])
-  
-  
+
+     useEffect(() => {
+        const fetchData = async () => {
+          const result = await axios(`${companyHistoricalUrl}${symbol}${Key.fmpk}`,);
+          //console.log("historical", result.data)
+            setCompanyHistorical(result.data.historical);
+            
+            }
+            fetchData();
+              }, [symbol])
+
+       //**************************************************//       
+       const priceGraphData = [
+         {
+        "id": "price",
+        "data": []
+      },
+      {
+        "id": "high",
+        "data": []
+      },
+      {
+        "id": "low",
+        "data": []
+      }
+      ]
+      companyHistorical.map(item => {
+        priceGraphData[0].data.push({x: item.date, y: item.close.toFixed(2)})
+        priceGraphData[1].data.push({x: item.date, y: item.high.toFixed(2)})
+        priceGraphData[2].data.push({x: item.date, y: item.low.toFixed(2)})
+      });
+     
+  console.log("companyHistoricalArray", companyHistorical)
+
+  const  fiveDay = [{
+    "id": "price",
+    "data": priceGraphData[0].data.slice(0, 5).reverse()
+  }, 
+  {
+    "id": "high",
+    "data": priceGraphData[1].data.slice(0,5).reverse()
+  }, {
+    "id": "low",
+    "data": priceGraphData[2].data.slice(0, 5).reverse()
+  }
+
+
+]
     return (
   <CompanyLayout>
     <SEO title="CompanyDetails" />
-    
-    <main className={companyStyles.mainContentCompanyContainer}>
     <h4 className={companyStyles.pageHeader}>COMPANY SUMMARY</h4>
-      <section className={companyStyles.halfMainContentContainer}>
+    <main className={companyStyles.mainContentCompanyContainer}>
+    
+      <section className={companyStyles.fiftyFiveMainContentContainer}>
     {(companyObject.profile && companyRating.rating) && <CompanySummary company={companyObject} companyRating={companyRating}/> }
     {(companyKeyMetrics.metrics && companyObject.profile) &&  <CompanyDataBrief company={companyObject} companyKeyMetrics={companyKeyMetrics}/> }
     </section>
+      <section className={companyStyles.fortyFiveMainContentContainer}>
+        <div className={companyStyles.chartContainer}>
+        <CompanyLineChart data={fiveDay}/>
+        </div>
+      </section>
    </main>
   </CompanyLayout>
     )
