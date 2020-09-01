9c13fadbeb15f173fa34aeea8527bdfc5e267dde
ocelotdive <oscar1dlh@hotmail.com>
2020-09-01 13:21:59 +0100

    fallback for non dataBrief items

--- src/components/companyDetails/CompanyRating.js
+++ src/components/companyDetails/CompanyRating.js
@@ -33,7 +33,10 @@ const CompanyRating = ({companyRating}) => {
       <div className={companyStyles.cardInfoBar} key={item}>&nbsp;&nbsp;&nbsp;
         <span className={companyStyles.cardDataName}>{item}</span>&nbsp;&nbsp;&nbsp;<span className={companyStyles.cardScore}><small>Score: </small> 
         {temp.map((score, index) => <small><small><FontAwesomeIcon icon={faStar} key={item + index}/></small></small>)}</span>
-        <span className={companyStyles.cardDataInfo}><small>Rec: </small><span className={companyStyles.recommendation}><small><small>{companyRating.ratingDetails[item].recommendation}</small></small></span></span>
+        <span className={companyStyles.cardDataInfo}><small>Rec: </small>
+        <span className={companyStyles.recommendation}><small><small>{companyRating.ratingDetails[item].recommendation}</small></small>
+        </span>
+        </span>
     </div>
       )
    })
