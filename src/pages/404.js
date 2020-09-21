8fb64a067956d5e1524ab7f6c41cb391c2685514
ocelotdive <oscar1dlh@hotmail.com>
2020-09-21 14:33:47 +0100

    chore: add home link to 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,5 @@
 import React from "react"
-
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -8,6 +8,14 @@ const NotFoundPage = () => (
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>
+      Use the back button or return to homepage.{" "}
+      <Link to="/">
+        <li className={navStyles.mainNavListItem}>
+          <span className={navStyles.navListLink}>Home</span>
+        </li>
+      </Link>
+    </p>
   </Layout>
 )
 
